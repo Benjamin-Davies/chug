7b665b2b8e7b5f54fb1988ea2804c8c0918c09b4
Benjamin Davies <bentendavies@gmail.com>
2025-03-20 21:13:37 +1300

    fix: avoid downloading bottles more than once

--- src/bottles.rs
+++ src/bottles.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, fs};
+use std::collections::BTreeMap;
 
 use data_encoding::HEXLOWER;
 use flate2::read::GzDecoder;
@@ -30,7 +30,8 @@ impl Formula {
         let cellar_path = dirs::cellar_dir()?;
         let bottle_path = cellar_path.join(&self.name).join(&self.versions.stable);
         if bottle_path.exists() {
-            fs::remove_dir_all(&bottle_path)?;
+            println!("Bottle {:?} already downloaded", self.name);
+            return Ok(());
         }
 
         let unzip = GzDecoder::new(&mut raw_data);
