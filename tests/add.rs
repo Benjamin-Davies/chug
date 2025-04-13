56edc78c91b4e1d2e4cb499346f16a02eae359a1
Benjamin Davies <bentendavies@gmail.com>
2025-04-14 11:54:40 +1200

    test: fix flaky ca-certificates test

--- tests/add.rs
+++ tests/add.rs
@@ -29,24 +29,7 @@ fn test_add_python() {
     let program = output.bin_dir().join("python3.12");
     let status = Command::new(program).arg("--version").status().unwrap();
     assert!(status.success());
-}
 
-#[test]
-fn test_add_certs() {
-    let output = output_dir::new();
-
-    let program = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("target/debug/chug");
-    let status = Command::new(program)
-        .args(["add", "httpie"])
-        .status()
-        .unwrap();
-    assert!(status.success());
-
-    let program = output.bin_dir().join("https");
-    let status = Command::new(program)
-        .arg("get")
-        .arg("api.github.com")
-        .status()
-        .unwrap();
-    assert!(status.success());
+    let ca_certs = output.data_dir().join("chug/etc/ca-certificates/cert.pem");
+    assert!(ca_certs.exists());
 }
